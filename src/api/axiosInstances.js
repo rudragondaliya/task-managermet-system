34f6137027257af7a04af86f9d25c4276f11d3ec
rudragondaliya <gondaliyarudra61@gmail.com>
2025-07-07 09:47:42 +0530

    done

--- src/api/axiosInstances.js
+++ src/api/axiosInstances.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
  export const axiosInstances = axios.create({
-    baseURL:import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    baseURL:import.meta.env.VITE_FIREBASE_DATABASE_URL + "todos",
 })
